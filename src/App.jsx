6856c231466dbb2cc298022bbbe31d4113646893
Waleed <wsaleh1991@gmail.com>
2025-12-17 19:44:29 -0600

    Component for navbar finished. No functionality yet.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+// Components
+import Navigation from "./components/Navbar/Navigation";
+
 import "./App.css";
 
 function App() {
   return (
     <>
       <div>
-        <h1>Hello World</h1>
-        <h1>here is my navbar</h1>
+        <Navigation />
       </div>
     </>
   );
