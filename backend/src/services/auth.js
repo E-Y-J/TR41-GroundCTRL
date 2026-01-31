86e7618b37d97f141f5416d29bfab7ffa86c91a4
Adam Colyer <adamcolyer@gmail.com>
2026-01-31 00:26:22 -0600

    syntax error 1000

--- backend/src/services/auth.js
+++ backend/src/services/auth.js
@@ -3,7 +3,9 @@
 module.exports = {
   async login({ _email, _password }) {
     return { token: 'dummy-token', refreshToken: 'dummy-refresh', uid: 'dummy-uid' };
+
   },
+};
   async register({ _email, _password }) {
     return { uid: 'dummy-uid', email: _email };
   },
